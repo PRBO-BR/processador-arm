9be2ed743a53d5827cab23c532ec2d7982a2c04c
Paulo Rafael Borges de Oliveira <concurseiropadrao@gmail.com>
2020-10-24 17:15:47 -0300

    Alteração do tamanho da variável ALUControl de 2 bits para 3

---
+++ typescript.ts
@@ -0,0 +1,9 @@
+add wave -position end  sim:/testbench/dut/arm/clk
+add wave -position end  sim:/testbench/dut/arm/Instr
+add wave -position end  sim:/testbench/dut/arm/ALUResult
+add wave -position 2  sim:/testbench/dut/arm/ALUFlags
+add wave -position 3  sim:/testbench/dut/arm/ALUControl
+add wave -position end  sim:/testbench/dut/arm/dp/rf/rf
+add wave -position 5  sim:/testbench/dut/arm/dp/rf/r15
+add wave -position 6  sim:/testbench/dut/arm/dp/alu/a
+add wave -position 7  sim:/testbench/dut/arm/dp/alu/b
